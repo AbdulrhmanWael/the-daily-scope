8562af7a9a65097b6baf1e331b9ac6b32a057871
AbdulrhmanWael <abdelrhmanelkholy296@gmail.com>
2025-09-07 19:16:59 +0300

    switch gnews api

--- apiKeys.js
+++ apiKeys.js
@@ -3,5 +3,6 @@ export const ALL_SPORTS_API_KEY =
   "2dd00d8f167c46a0ba3cbb154eceb4dbaffab5ad7da7edff7b8ed4fad5595ba0";
 export const API_FOOTBALL_KEY = "de7ef924ac13f4113626f371b39e8fa7";
 export const GET_GEO_API_KEY = "1bbc14ffa328e6d7f9417cd833929ebfc0994a34";
-export const GNEWS_API_KEY = "97a2668493e89b71d83fcb75859c7971";
-//"bbe243be8799b4f3cba6a71203111051";
+export const GNEWS_API_KEY = "bbe243be8799b4f3cba6a71203111051"; 
+//"97a2668493e89b71d83fcb75859c7971";
+
